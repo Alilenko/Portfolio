2b8f5ea06582f01e6ec8b6b0900c8aea9aeed268
Alina <pavlenkoalina9694@gmail.com>
2021-12-15 18:53:13 +0200

    add project

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,7 +3,7 @@ import './project.css';
 import shop from '../../img/shop.png';
 import todo from '../../img/todo.png'
 import conv from '../../img/conv.png'
-
+import hunger from '../../img/hunger.png'
 
 const Project = () => {
     return (
@@ -48,12 +48,35 @@ const Project = () => {
                     </div>
                     <div className="card-content">
                     <span className="card-title">Конвертор валют</span>
-                    <p>Приложение конвертор валют</p>
+                    <p>Приложение конвертор валют. В проекте реализованно: получение данных из API с помощью fetch.
+                        использование хуков useState, useEffect.
+                        получение значения от пользователя и конвертация в четыре валюты.</p>
  
                     </div>
                     <div className='card-buttons'>
                         <a href='https://codepen.io/Alilenko/pen/qBXXrNv' className='link'>Посмотреть</a>
                         <a href='https://github.com/Alilenko/converter' className='link'>Посмотреть код</a>
+                     </div>
+                </div>
+                <div className="card">
+                    <div className="card-image">
+                    <img src={hunger}/>
+                    
+                    </div>
+                    <div className="card-content">
+                    <span className="card-title">Landing Page страница ресторана</span>
+                    <p>HTML, CSS, JavaScript. В проекте  реализованно: Адаптивная верстка.
+                        Фиксированный header который исчезает при скролле вниз и выезжает при прокрутке вверх.
+                        Слайдер с помощью библиотеки Swiper.
+                        При клике на кнопки навигации плавная прокрутка к якорю.
+                        Секция меню которая меняется в зависимости от выбраной категории.
+                        Галерея.
+                        добавленна Google карта.</p>
+ 
+                    </div>
+                    <div className='card-buttons'>
+                        <a href='https://alilenko.github.io/Restaurant/' className='link'>Посмотреть</a>
+                        <a href='https://github.com/Alilenko/Restaurant/tree/main' className='link'>Посмотреть код</a>
                      </div>
                 </div>
             </div>
