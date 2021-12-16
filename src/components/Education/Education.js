58cde3dc5ee99c517b71f0c5a525461b59bc8144
Alina <pavlenkoalina9694@gmail.com>
2021-12-16 19:39:14 +0200

    add links

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -7,11 +7,11 @@ const Education = () => {
             <h2 className='title'>Educations</h2>
             <div className='title_content'> 
             
-                    <div className='text'><span>2021</span> JavaScript + React. Иван Петриченко</div>
-                    <div className='text'><span>2021</span> React для начинающих. Михаил Непомнящий</div> 
-                    <div className='text'><span>2021</span> WEB-разработчик. Иван Петриченко</div>
-                    <div className='text'><span>2021</span> Курс по JavaScript. Евгений Андриканич</div>
-                    <div className='text'><span>2020</span> Курс по верстке HTML,CSS. Евгений Андриканич</div>
+                    <div className='text'><span>2021</span><a href='https://www.udemy.com/course/javascript_full/'>JavaScript + React.</a></div>
+                    <div className='text'><span>2021</span><a href='https://www.udemy.com/course/react-from-scratch/'> React для начинающих.</a> </div> 
+                    <div className='text'><span>2021</span><a href='https://www.udemy.com/course/webdeveloper/'> WEB-разработчик.</a> </div>
+                    <div className='text'><span>2021</span><a href='https://www.youtube.com/watch?v=wdViO9OcQzs&list=PLM6XATa8CAG4F9nAIYNS5oAiPotxwLFIr&index=36'> Курс по JavaScript. </a></div>
+                    <div className='text'><span>2020</span><a href='https://www.youtube.com/watch?v=z3GS5oYGq5U&list=PLM6XATa8CAG4F9nAIYNS5oAiPotxwLFIr&index=3'> Курс по верстке HTML,CSS. </a> </div>
                     </div>
             <div className='subtitle'>Высшее образование:</div>
                 <div className='text'>
