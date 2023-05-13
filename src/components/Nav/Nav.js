4ce56167c4397433ec48fdaff9c5556b75df4584
Alina <pavlenkoalina9694@gmail.com>
2023-05-13 15:20:49 +0300

    fix nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import './nav.css';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import "./nav.css";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div className='nav'>
-            <NavLink exact activeClassName='nav__active' to='/' className='nav__link'>Contact</NavLink>
-            <NavLink activeClassName='nav__active' to='/about' className='nav__link'>About</NavLink>
-            <NavLink activeClassName='nav__active' to='/education' className='nav__link'>Education</NavLink>
-            <NavLink activeClassName='nav__active' to='/project' className='nav__link'>Project</NavLink>
-        </div>
-    )
-}
+  return (
+    <div className="nav">
+      <NavLink exact activeClassName="nav__active" to="/" className="nav__link">
+        Contact
+      </NavLink>
+      {/*<NavLink activeClassName='nav__active' to='/about' className='nav__link'>About</NavLink>*/}
+      <NavLink
+        activeClassName="nav__active"
+        to="/education"
+        className="nav__link"
+      >
+        Education
+      </NavLink>
+      <NavLink
+        activeClassName="nav__active"
+        to="/project"
+        className="nav__link"
+      >
+        Project
+      </NavLink>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
